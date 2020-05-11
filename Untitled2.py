90b0b01a8ecba344a72b922a2258890d27cbcf1a
ANKUSHTELMASRE <64907538+ANKUSHTELMASRE@users.noreply.github.com>
2020-05-11 19:59:19 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+name='ankush'
+
+
+# In[2]:
+
+
+type(name)
+
+
+# In[3]:
+
+
+print(name.title())
+
+
+# print(namne.upper())
+
+# In[4]:
+
+
+print(name.upper())
+
+
+# In[5]:
+
+
+print(name.lower())
+
+
+# In[6]:
+
+
+firstnamne='ankush'
+lastname='telmasre'
+
+
+# In[7]:
+
+
+fullname=f"{firstname}{lastname}"
+
+
+# In[8]:
+
+
+fullname= f"{firstname}{lastname}"
+
+
+# In[9]:
+
+
+print(fullname)
+
+
+# In[13]:
+
+
+firstname='ankush'
+lastname='telmasre'
+fullname=f"{firstname}{lastname}"
+print(fullname)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
